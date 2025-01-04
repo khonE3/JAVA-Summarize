befba18773448c156566e7eb38ac2aa62d312143
E3Coder <gotgothot.99@gmail.com>
2025-01-04 22:48:03 +0700

    Concept JAVA
    JAVA summarize

---
+++ ConceptJAVA.java
@@ -0,0 +1,21 @@
+public class ConceptJAVA {
+    public static void main(String[] args) {
+        // Variable
+        // type variableName = value;
+        char _one = 'J';
+        String _str = "osepha";
+        int This_Integer = 3;
+        float This_Float = 1.66f; // ต้องมี f
+        double This_Double = 3.3456456;
+        final int Const = 15; // final = const
+
+        //Print OUTPUT
+        //print
+        System.out.print(_one);
+        //println
+        System.out.println(_str);
+        System.out.println(This_Integer + Const + This_Double);
+        //printf
+        System.out.printf("Float ไม่สามารถบวก + Double Flaot = %f " , This_Float);
+    }
+}
